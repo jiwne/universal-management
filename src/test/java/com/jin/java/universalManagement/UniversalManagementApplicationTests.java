c04bdfa9ae4a61d156d31d00eaefbacda70f09b9
jiwne <142486336+jiwne@users.noreply.github.com>
2025-04-30 10:32:30 +0800

    init code

---
+++ src/test/java/com/jin/java/universalManagement/UniversalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.java.universalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
