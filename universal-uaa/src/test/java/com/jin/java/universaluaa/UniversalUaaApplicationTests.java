68456d3345145c22c9b6b3604d9f7e1309da01f4
jinwenbin <jinwenbin@e-byte.com>
2025-04-30 11:55:13 +0800

    init uaa

---
+++ universal-uaa/src/test/java/com/jin/java/universaluaa/UniversalUaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.java.universaluaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalUaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
