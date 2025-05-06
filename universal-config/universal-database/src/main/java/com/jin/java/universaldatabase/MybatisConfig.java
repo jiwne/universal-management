3453271f64d49d6ed2a91e0d1a5b1fc513a0a836
jinwenbin <jinwenbin@e-byte.com>
2025-05-06 10:17:59 +0800

    add:database config

---
+++ universal-config/universal-database/src/main/java/com/jin/java/universaldatabase/MybatisConfig.java
@@ -0,0 +1,21 @@
+package com.jin.java.universaldatabase;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author：jin
+ * @date：2025/5/6
+ */
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
